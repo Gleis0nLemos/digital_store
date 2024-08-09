527c7c40dc08207d243a6a9780ef226287339673
Gleison Lemos <gleison04lemos@gmail.com>
2024-08-09 11:12:53 -0300

    feat: layout my information finish

--- src/components/MyInformations.jsx
+++ src/components/MyInformations.jsx
@@ -1,12 +1,33 @@
+// src/components/Information.jsx
 import React from 'react';
 
-const Information = () => {
+const MyInformation = () => {
   return (
-    <div>
-      <h2 className="text-2xl font-bold mb-4">Minhas Informações</h2>
-      <p>Informações pessoais...</p>
+    <div className="relative">
+      <h1 className='font-semibold text-sm text-dark-gray-2 pt-5'>Minhas Informações</h1>
+      <button className="absolute underline underline-offset-2 text-sm font-semibold top-2 right-2 text-primary px-2 py-2 rounded hover:text-tertiary">
+        Editar
+      </button>
+      <hr className="border-2 border-light-gray-3 mt-6 mb-2" />
+
+      <div className="mb-6 mt-8">
+        <h2 className="text-dark-gray-2 font-bold mb-2">Informações Pessoais</h2>
+        <p><strong className='text-light-gray pr-2'>Nome:</strong> João Silva</p>
+        <p><strong className='text-light-gray pr-2'>CPF:</strong> 123.456.789-00</p>
+        <p><strong className='text-light-gray pr-2'>Email:</strong> joao.silva@email.com</p>
+        <p><strong className='text-light-gray pr-2'>Celular:</strong> (11) 98765-4321</p>
+      </div>
+
+      <div>      
+        <hr className="border-2 border-light-gray-3 mt-6 mb-6" />
+        <h2 className="text-dark-gray-2 font-bold mb-2">Informações de Entrega</h2>
+        <p><strong className='text-light-gray pr-2'>Endereço:</strong> Rua das Flores, 123</p>
+        <p><strong className='text-light-gray pr-2'>Bairro:</strong> Jardim das Acácias</p>
+        <p><strong className='text-light-gray pr-2'>Cidade:</strong> São Paulo</p>
+        <p><strong className='text-light-gray pr-2'>CEP:</strong> 01234-567</p>
+      </div>
     </div>
   );
 };
 
-export default Information;
+export default MyInformation;
--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -1,4 +1,3 @@
-// src/components/SideMenu.jsx
 import React from 'react';
 import PropTypes from 'prop-types';
 
@@ -11,16 +10,16 @@ const SideMenu = ({ selectedMenu, setSelectedMenu }) => {
   ];
 
   return (
-    <nav className="w-[370px] bg-white p-5 shadow-sm">
+    <nav className="w-[370px] h-full bg-white p-5 pb-8 shadow-sm">
       <ul>
-        {menuItems.map(item => (
+        {menuItems.map((item, index) => (
           <li
             key={item.id}
-            className={`p-2 cursor-pointer ${selectedMenu === item.id ? 'text-primary font-bold' : ''}`}
+            className={`py-4 px-4 cursor-pointer ${selectedMenu === item.id ? 'text-primary font-bold' : ''}`}
             onClick={() => setSelectedMenu(item.id)}
           >
             {item.label}
-            <hr className="border-2 border-light-gray-3 my-2" />
+            {index < menuItems.length - 1 && <hr className="border-2 border-light-gray-3 mt-6" />}
           </li>
         ))}
       </ul>
