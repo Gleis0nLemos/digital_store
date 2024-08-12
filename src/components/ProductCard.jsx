d31a02197459e7df2b1e8c72e7618844b29cba75
Gleison Lemos <gleison04lemos@gmail.com>
2024-08-12 16:50:32 -0300

    feat: dinamic discount added

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -8,11 +8,29 @@ const ProductCard = ({ id, image, category, name, price, priceDiscount }) => {
     navigate(`/products/${id}`);
   };
 
+  const calculateDiscountPercentage = (price, priceDiscount) => {
+    if (price && priceDiscount && price > 0) {
+      const discountPercentage = Math.round(((price - priceDiscount) / price) * 100);
+      return discountPercentage;
+    }
+    return 0;
+  };
+
+  // Calcula a porcentagem de desconto
+  const discountPercentage = priceDiscount ? calculateDiscountPercentage(Number(price), Number(priceDiscount)) : null;
+
   return (
     <div onClick={handleClick} className="cursor-pointer">
-      <div className="mb-1 md:mb-4 min-h-[187px] max-h-[282px] md:max-w-[275px] md:h-[287px] xl:w-[279px] xl:h-[292px] flex items-center bg-white shadow-sm hover:underline">
+      
+      <div className="relative mb-1 md:mb-4 min-h-[187px] max-h-[282px] md:max-w-[275px] md:h-[287px] xl:w-[279px] xl:h-[292px] flex items-center bg-white shadow-sm hover:underline">
+      
+
+        {discountPercentage !== null && (
+          <p className="absolute top-2 left-2 ml-2 mt-2 bg-lime text-sm px-4 p-2 inline-block rounded-full text-center text-dark-gray-2 font-bold">{discountPercentage}% OFF</p>
+        )}
         <img src={image} alt={name} className="w-auto h-auto object-cover" />
       </div>
+      
       <div className="mb-2 md:mb-4">
         <h3 className="text-light-gray font-inter text-[12px] font-bold leading-6 tracking-[0.75px] text-left">
           {category}
